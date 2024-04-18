f085f0f4e875954757a67cf60bc35c1f7cf96a76
Sai Madhavan G <g.saimadhavan@gmail.com>
2024-04-18 14:46:26 +0530

    dqn agent done

---
+++ environment.py
@@ -0,0 +1,31 @@
+import gym
+
+
+class Envionment:
+    """
+    A wrapper class around gym.env
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def reset(self):
+        pass
+
+    def step(self, action):
+        pass
+
+
+class CartPoleEnvironment(Envionment):
+    def __init__(self, render_mode="") -> None:
+        super().__init__()
+        if render_mode:
+            self.env = gym.make("CartPole-v1", render_mode=render_mode)
+        else:
+            self.env = gym.make("CartPole-v1")
+
+    def reset(self):
+        return self.env.reset()
+
+    def step(self, action):
+        self.env.step(action)
