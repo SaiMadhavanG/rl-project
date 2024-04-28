688ed5c3bd3ac8ba256a1cde161aa37d01575a31
Sai Madhavan G <g.saimadhavan@gmail.com>
2024-04-29 00:07:52 +0530

    cartpole tde with is done

--- tde-IS-weights/train.py
+++ tde-IS-weights/train.py
@@ -7,11 +7,11 @@ import torch
 
 device = "cuda"
 
-agent = DQNAgent(4, 8, device=device)
-env = LunarLanderEnvironment()
-render_env = LunarLanderEnvironment("render")
-logger = Logger("Lunar_Lander_tde_IS_baseline", path="../cartpole/runs/")
-optimizer = torch.optim.Adam(agent.network.parameters(), lr=1e-3)
+agent = DQNAgent(2, 4, device=device)
+env = CartPoleEnvironment()
+render_env = CartPoleEnvironment("human")
+logger = Logger("cartpole-tde-is")
+optimizer = torch.optim.Adam(agent.network.parameters(), lr=5e-4)
 powerReplay = PowerReplay(5e3, 32, 1, {"tde_alpha": 0.6}, "tde")
 
 trainer = AgentTrainer(
@@ -21,12 +21,12 @@ trainer = AgentTrainer(
     optimizer,
     powerReplay,
     device=device,
-    epsilon=0.7,
+    epsilon=0.5,
     min_epsilon=0.1,
-    epsilon_decay=8e-4,
+    epsilon_decay=5e-4,
     gamma=1,
     update_frequency=5,
 )
 
 # pdb.set_trace()
-trainer.train_steps(2e5)
+trainer.train_steps(1e5)
