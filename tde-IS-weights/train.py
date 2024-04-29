e1bf0dca6ccca4bb53e78b86d69dd56ace60cd1b
Sai Madhavan G <g.saimadhavan@gmail.com>
2024-04-29 10:07:35 +0530

    corrected cartpole tde done

--- tde-IS-weights/train.py
+++ tde-IS-weights/train.py
@@ -10,9 +10,9 @@ device = "cuda"
 agent = DQNAgent(2, 4, device=device)
 env = CartPoleEnvironment()
 render_env = CartPoleEnvironment("human")
-logger = Logger("cartpole-returns-is")
+logger = Logger("cartpole-tde-is-2")
 optimizer = torch.optim.Adam(agent.network.parameters(), lr=5e-4)
-powerReplay = PowerReplay(5e3, 32, 1, {"estimatedReturn_alpha": 1}, "returns")
+powerReplay = PowerReplay(5e3, 32, 1, {"tde_alpha": 0.6}, "tde")
 
 trainer = AgentTrainer(
     agent,
